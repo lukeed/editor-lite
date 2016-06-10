bf0baed287e33d3b636707e5a09bb9b714470750
Luke Edwards <luke.edwards05@gmail.com>
2016-06-10 15:23:26 -0700

    add extend helper (shallow)

--- src/index.js
+++ src/index.js
@@ -2,5 +2,21 @@
 
 var domsel = require('dom-selection');
 
-module.exports = function (str) {
-};
+// Array prototype shortnames
+var slice = [].slice;
+
+/**
+ * Shallow Extend
+ * @param  {Object} tar
+ * @return {Object}
+ */
+function extend(tar) {
+	slice.call(arguments, 1).forEach(function (src) {
+		for (var k in src) {
+			if (src.hasOwnProperty(k)) {
+				tar[k] = src[k];
+			}
+		}
+	});
+	return tar;
+}
